1d50f98c033487481c34ec68c61f19065f245d02
ZeNerd234 <98205955+ZeNerd234@users.noreply.github.com>
2022-01-22 16:22:09 +0530

    Minor changes
    an implementation of graphics and simulation

--- stemsim.py
+++ stemsim.py
@@ -8,8 +8,9 @@ black=(0,0,0)
 brown=(139,69,19)
 green=(0,255,0)
 red=(255,0,0)
-blue=(0,0,255)
+blue=(30,144,255)
 white=(255,255,255)
+yellow=(255,255,0)
 screen=pygame.display.set_mode((600,600))
 WIDTH=screen.get_width()
 HEIGHT=screen.get_height()
@@ -17,36 +18,85 @@ class FrictionSurf(pygame.sprite.Sprite):
     def __init__(self, color, friction, bounce):
         super().__init__()
         self.friction=friction
-        self.surf=pygame.Surface((WIDTH,10))
+        self.surf=pygame.Surface((WIDTH,20))
         self.surf.fill(color)
-        self.rect=self.surf.get_rect()
+        self.rect=self.surf.get_rect(bottomleft=(0,HEIGHT))
         self.bounce=bounce
 
 
 class Body(pygame.sprite.Sprite):
-    def __init__(self, velocity, acceleration, position, color, size):
-        super.__init__()
+    def __init__(self, velocity, acceleration, position, color, size, fixed_acc=0.5):
+        super().__init__()
         self.surf=pygame.Surface(size)
         self.surf.fill(color)
-        self.rect=self.surf.get_rect(center=(position))
+        self.pos=vec(position)
+        self.rect=self.surf.get_rect(bottomright=(self.pos))
         self.vel=vec(velocity)
         self.acc=vec(acceleration)
+        self.ACC=fixed_acc
+
         
     def changevel(self,surface):
-        if pygame.sprites.spritecollideany(self,surface):
-            self.vel.y = -self.vel.y+surface.bounce
+        if pygame.sprite.spritecollideany(self,surface):
+            self.friction=-0.1
+            if self.vel.y!=0:
+                self.vel.y = -self.vel.y-surface.bounce
 
+            
+
+    def move(self):
+        pk=pygame.key.get_pressed()
+        if pk[K_LEFT]:
+            self.acc.x = -self.ACC
+        if pk[K_RIGHT]:
+            self.acc.x = self.ACC
+        self.acc.x += self.vel.x * self.friction
+        self.vel += self.acc
+        if not self.vel==0:
+            self.pos += self.vel + 0.5 * self.acc
+            self.rect.bottomright=self.pos
+        if self.rect.x<0:
+            self.rect.x=WIDTH
+        if self.rect.x>WIDTH:
+            self.rect.x=0
         
-
-while True: 
+                
+        
+#wood=FrictionSurf(brown,-0.5,0.1)               
+#glass=FrictionSurf(blue,-0.2,0.5)
+#sand=FrictionSurf(yellow, -0.7,0.7)
+sprites=pygame.sprite.Group()
+font=pygame.font.SysFont('Courier',20)
+w=font.render('Wood',True,black)
+g=font.render('Glass',True,black)
+s=font.render('Sand',True,black)
+b=Body(vec(0,0),vec(0,0),(50,HEIGHT-20),green,(30,30))
+wood=FrictionSurf(brown,-0.1,0.1)
+sprites=pygame.sprite.Group(b)
+obs=pygame.sprite.Group(wood)
+b.friction=wood.friction
+while True:
     for event in pygame.event.get():
         if event.type==QUIT:
             pygame.quit()
             quit()
+        '''if event.type==KEYDOWN:
+            if event.key==K_UP:'''
+            
     
     screen.fill(white)
+    pygame.draw.rect(screen,blue,[50,50,50,20])
+    screen.blit(w,(50,50))
+    pygame.draw.rect(screen,blue,[150,50,60,20])
+    screen.blit(g,(150,50))
+    pygame.draw.rect(screen,blue,[250,50,50,20])
+    screen.blit(s,(250,50))
+    b.move()
+    b.changevel(obs)
+    screen.blit(wood.surf,wood.rect)
+    for sprite in sprites:
+        screen.blit(sprite.surf,sprite.rect)
+    pygame.display.update()
+    clock.tick(60)
 
     
-
-
-
