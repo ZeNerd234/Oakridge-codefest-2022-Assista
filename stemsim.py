02d4dc393fc45e6032dd982e11d941179a3de94e
ZeNerd234 <98205955+ZeNerd234@users.noreply.github.com>
2022-01-22 12:56:14 +0530

    Add files via upload

---
+++ stemsim.py
@@ -0,0 +1,11 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+pygame.init()
+black=(0,0,0)
+brown=(139,69,19)
+green=(0,255,0)
+red=(255,0,0)
+blue=(0,0,255)
+screen=pygame.display.set_mode((600,600))
+
