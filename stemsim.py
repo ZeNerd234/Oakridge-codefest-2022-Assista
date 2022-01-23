67f5bb79951d0660f34184dada79710cb01645de
ZeNerd234 <98205955+ZeNerd234@users.noreply.github.com>
2022-01-23 10:29:29 +0530

    final implementation

--- stemsim.py
+++ stemsim.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+
 vec=pygame.math.Vector2
 class FrictionSurf(pygame.sprite.Sprite):
     def __init__(self, color, friction, bounce):
@@ -24,12 +25,15 @@ class Body(pygame.sprite.Sprite):
         self.acc=vec(acceleration)
         self.ACC=fixed_acc
         self.jumping=False
+        self.grav=0.1
 
         
     def changevel(self,surface):
+        self.surface=surface
         if pygame.sprite.spritecollideany(self,surface):
             if self.vel.y!=0:
                 self.vel.y =0
+                self.jumping=False
 
             
 
@@ -39,6 +43,10 @@ class Body(pygame.sprite.Sprite):
             self.acc.x = -self.ACC
         if pk[K_RIGHT]:
             self.acc.x = self.ACC
+        if pk[K_UP]:
+            if pygame.sprite.spritecollideany(self,self.surface):
+                self.vel.y=-8
+            self.jumping=True
         self.acc.x += self.vel.x * self.friction
         self.vel += self.acc
         if not self.vel==0:
@@ -46,8 +54,12 @@ class Body(pygame.sprite.Sprite):
             self.rect.bottomright=self.pos
         if self.rect.x<0:
             self.rect.x=0
-        if self.rect.x>600:
-            self.rect.x=600
+        if self.rect.right>600:
+            self.rect.right=600
+        if self.jumping:
+            self.vel.y+=self.grav
+        if self.vel.y==0:
+            self.jumping=False
 
         
                 
@@ -74,10 +86,14 @@ def simulate():
     HEIGHT=screen.get_height()
     sprites=pygame.sprite.Group()
     font=pygame.font.SysFont('Courier',20)
+    font2=pygame.font.SysFont('Courier',15)
     w=font.render('Wood',True,black)
     g=font.render('Glass',True,black)
     s=font.render('Sand',True,black)
     t=font.render('Stop',True,black)
+    r=font.render('Gravity',True,black)
+    plus=font.render('+',True,black)
+    minus=font.render('-',True,black)
     b=Body(vec(0,0),vec(0,0),(50,HEIGHT-20),green,(30,30))
     wood=FrictionSurf(brown,-0.5,0.1)
     b.friction=wood.friction
@@ -102,7 +118,19 @@ def simulate():
                 if 350<mouse[0]<400 and 50<mouse[1]<70:
                     pygame.quit()
                     return
+                if 450<mouse[0]<465 and 75<mouse[1]<90:
+                    if b.grav<0.9:
+                        b.grav+=0.1
+                    else:
+                        b.grav=0.9
+                if 525<mouse[0]<540 and 75<mouse[1]<90:
+                    if b.grav>0.1:
+                        b.grav-=0.1
+                    else:
+                        b.grav=0.1
         screen.fill(white)
+        gv=int(round(b.grav*10))
+        gr=font2.render(str(gv),True,black)
         pygame.draw.rect(screen,blue,[50,50,50,20])
         screen.blit(w,(50,50))
         pygame.draw.rect(screen,blue,[150,50,60,20])
@@ -111,6 +139,12 @@ def simulate():
         screen.blit(s,(250,50))
         pygame.draw.rect(screen,red,[350,50,50,20])
         screen.blit(t,(350,50))
+        screen.blit(r,(450,50))
+        pygame.draw.rect(screen,grey,[450,75,15,15])
+        pygame.draw.rect(screen,grey,[525,75,15,15])
+        screen.blit(plus,(450,70))
+        screen.blit(minus,(525,70))
+        screen.blit(gr,(475,75))
         mouse=pygame.mouse.get_pos()
         b.move()
         b.changevel(obs)
